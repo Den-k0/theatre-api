f0b15b646d7e6062f39427a6001ee9e30a39f2fc
Den_k0 <denys.kotovskiy@gmail.com>
2024-12-24 14:39:39 +0200

    Add property full_name & capacity to models Actor & TheatreHall

--- theatre/models.py
+++ theatre/models.py
@@ -8,6 +8,10 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
@@ -33,6 +37,10 @@ class TheatreHall(models.Model):
     name = models.CharField(max_length=255)
     rows = models.PositiveIntegerField()
     seats_in_row = models.PositiveIntegerField()
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
 
     def __str__(self):
         return self.name
